c14b7096f48bf87d360ca517407128dbf2a7af39
maxwangatgit <1509119237@qq.com>
2020-10-18 16:25:11 +0800

    vscode update测试

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2020-10-18 15:27:27
-LastEditTime: 2020-10-18 16:21:57
+LastEditTime: 2020-10-18 16:24:16
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \learnPytorch\test.py
@@ -10,3 +10,4 @@ test
 二三年级健康就好 吗顺丰到付风水大师说过
 你好吗，我很好，我还乐意更好，你想变得怎么样
 你快乐吗
+还好吧，还能凑合过
